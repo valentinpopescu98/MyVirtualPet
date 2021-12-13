e4215062eda2147d041df1cbb4495f596bd8627b
vali <valentinpopescu98@gmail.com>
2021-12-13 11:54:41 +0200

    Final update for sprint 1

--- Assets/Scripts/Character/ActivateCharacter.cs
+++ Assets/Scripts/Character/ActivateCharacter.cs
@@ -12,14 +12,14 @@ public class ActivateCharacter : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                //if (hit.transform.tag == "Character")
-                //{
-                //    TransformCharacter objectScript = hit.collider.GetComponent<TransformCharacter>();
-                //    objectScript.isOneActive = !objectScript.isOneActive;
-                //}
-            }
+            //if (Physics.Raycast(ray, out hit))
+            //{
+            //    if (hit.transform.tag == "Character")
+            //    {
+            //        TransformCharacter objectScript = hit.collider.GetComponent<TransformCharacter>();
+            //        objectScript.isOneActive = !objectScript.isOneActive;
+            //    }
+            //}
         }
 
         if (Input.touchCount == 2 && Input.touches[0].phase == TouchPhase.Began && Input.touches[1].phase == TouchPhase.Began)
@@ -31,11 +31,11 @@ public class ActivateCharacter : MonoBehaviour
             Ray ray2 = Camera.main.ScreenPointToRay(Input.touches[1].position);
             RaycastHit hit1, hit2;
 
-            if (Physics.Raycast(ray1, out hit1) && Physics.Raycast(ray2, out hit2))
-            {
-                //TransformCharacter objectScript = GameObject.FindGameObjectWithTag("Character").GetComponent<TransformCharacter>();
-                //objectScript.isTwoActive = !objectScript.isTwoActive;
-            }
+            //if (Physics.Raycast(ray1, out hit1) && Physics.Raycast(ray2, out hit2))
+            //{
+            //    TransformCharacter objectScript = GameObject.FindGameObjectWithTag("Character").GetComponent<TransformCharacter>();
+            //    objectScript.isTwoActive = !objectScript.isTwoActive;
+            //}
         }
     }
 }
